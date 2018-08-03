64e501d4da302730fcdbc6c8ae1d78e4b2906b88
Kazi Jahurul Islam <jahurul.sust@gmail.com>
2018-08-03 16:44:54 +0600

    Adding svg-wp.php file

---
+++ svg-wp.php
@@ -0,0 +1,17 @@
+<?php defined('ABSPATH') or die;
+/*
+Plugin Name: SVG WP
+Description: SVG Support WordPress Plugin
+Version: 1.0.0
+Author: Kazi Jahurul Islam
+Author URI: https://github.com/KaziJahurul
+Plugin URI: http://wordpress.org/plugins/svg-wp
+License: GPLv2 or later
+Text Domain: svg_wp
+Domain Path: /resources/languages
+*/
+
+
+define('SVG_WP_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('SVG_WP_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('SVG_WP_PLUGIN_VERSION', '1.0.0');
